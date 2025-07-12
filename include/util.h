b8e227734531a07ee816943c385cbdf55c429356
myferr <contactme.myfer@protonmail.com>
2025-07-12 18:26:58 -0500

    feat(*.h): add include/util.h

---
+++ include/util.h
@@ -0,0 +1,7 @@
+#pragma once
+
+char *prompt_password(const char *prompt);
+void print_banner(void);
+void print_help(const char *progname);
+void print_error(const char *msg);
+void print_info(const char *msg);
